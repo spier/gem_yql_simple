d6199d431644a114c1f85174c434ad00b8e14c4c
Sebastian Spier <blog@airness.de>
2012-05-07 00:09:46 +0200

    Fixing gem usage example

--- examples/gem_usage.rb
+++ examples/gem_usage.rb
@@ -2,16 +2,22 @@
 require "rubygems"
 require "bundler/setup"
 
-require "yql_simple"
+Bundler.require
+
+require "logger"
+
+log = Logger.new(STDOUT)
 
 #
 # Example of doing a twitter search via YQL
 #
+log.info "creating YqlSimple::SimpleClient and fetching some data from Twitter via YQL"
+simple_client = YqlSimple::SimpleClient.new()
 
 query_string = 'SELECT * FROM twitter.search WHERE q="#ruby" LIMIT 5'
-response = YqlSimple.query(query_string)
+response = simple_client.query(query_string)
 
-response["query"]["results"]["results"].each do |tweet| 
-  date = Time.parse(tweet["created_at"]).strftime("%Y-%m-%d %H:%M")  
-  puts "[#{date}] #{tweet["from_user"]}: #{tweet["text"]}"
+response["query"]["results"]["results"].each do |tweet|
+  date = Time.parse(tweet["created_at"]).strftime("%Y-%m-%d %H:%M")
+  log.info "[#{date}] #{tweet["from_user"]}: #{tweet["text"]}"
 end
